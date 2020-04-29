3d4ab22fbd1eb45b459b8f4b021376d58d1180d3
Gabi <gabrielaradu.developer@gmail.com>
2020-04-29 19:25:30 +0300

    Change label.

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,11 +6,8 @@ class Main extends Component {
         return (
             <div className="container">
                 <div className="jumbotron row">
-                    <h2 className="m-auto">Hmm... you seem to be lost. Go back <a href="http://gabrielaradu.me">home.</a></h2>
+                    <h2 className="m-auto">This is the main component that does nothing</h2>
                 </div>
-                <a href="http://gabrielaradu.me/" rel="noopener noreferrer" target="_blank" className="row">
-                    <img src={require('../img/undraw_page_not_found.svg')} alt="Invitation was sent with success"/>
-                </a>
             </div>
         );
     }
